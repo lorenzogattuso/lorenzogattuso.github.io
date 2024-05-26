6a67c695c247605ced9d6a0eed5690259f4fdebe
lorenzogattuso <lorenzo.gattuso.s@iisenzoferrari.it>
2024-05-26 22:43:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const themeSwitch = document.getElementById('themeSwitch');
+const darkElements = document.querySelectorAll('.about-me, .what-im-doing, .works');
+
+themeSwitch.addEventListener('change', () => {
+    // Cambio del tema globale
+    document.body.classList.toggle('dark-theme');
+    
+    // Cambio del wallpaper
+    if (document.body.classList.contains('dark-theme')) {
+        document.body.style.backgroundImage = "url('dark_wallpaper.jpg')";
+    } else {
+        document.body.style.backgroundImage = "url('light_wallpaper.jpg')";
+    }
+});
